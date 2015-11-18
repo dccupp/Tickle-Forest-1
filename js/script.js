b416675e7cd43676b49744596bf5e1b7b3f2fbb9
MrKnee <Adam.L.Knee@gmail.com>
2015-11-18 10:31:49 -0500

    Update to Animation and Art
    Dennis made some sweet updates to the animation. He added diagonal
    movement and fixed animation speeds to match up. He also inserted
    updated commenting for Adam to grasp some of the new things he did.
    Adam made some art updates to the HTML background and started making the
    bear sprite sheet and bear movements.

--- js/script.js
+++ js/script.js
@@ -1,97 +1,226 @@
-var number = 0; //randomly generated number that is passed to ghostBehavior function
-var counter = 0; //conditional number for ghostBehavior function while loop
-var behaveArray = new Array(); // array for storing animal behavioral functions
-var behaveIndex = 0; //number of behavioral functions the animal is able to use for the current movement action
+/**
+ * The Squirrel class is a blueprint for all squirrel class animals used on the page
+ * @author Dennis Cupp & Adam Knee
+ * @version 1.0, November 2015
+ *
+ * @param id    string - id of the animal
+ * @param xpos  int - horizontal starting position
+ * @param ypos  int - vertical starting position
+ * @param upNum int - number used to track squirrel's position relative to the top of the grid
+ * @param downNum int - number used to track squirrel's position relative to the bottom of the grid
+ * @param leftNum int - number used to track squirrel's position relative to the left side of the grid
+ * @param rightNum int - number used to track squirrel's position relative to the right side of the grid
+ */
 
-//postional variables
-var upCounter = 0; //variable used to track object's position relative to the top of the movement grid
-var downCounter = 0; //variable used to track object's position relative to the bottom of the movement grid
-var leftCounter = 0; //variable used to track object's position relative to the left of the movement grid
-var rightCounter = 0; //variable used to track object's position relative to the right of the movement grid
+function Squirrel(id, xPos, yPos, upNum, downNum, leftNum, rightNum){
+    //properties to let each animal store its own x/y position
+    this.x = xPos;
+    this.y = yPos;
+    //properties to track the position of the animal on the movement grid
+    this.upCounter = upNum;
+    this.downCounter = downNum;
+    this.leftCounter = leftNum;
+    this.rightCounter = rightNum;
+    //the ID of the animal
+    this.id = id;
+    //randomly generated number that is passed to squirrelBehavior function
+    this.number = 0; 
+    //property that keeps track of the item on the screen
+    this.animal_on_page;
+    //array for storing animal behavioral functions
+    this.behaveArray = new Array();
+    //number of behavioral functions the animal is able to use for the current movement action
+    this.behaveIndex = 0;
+    var thisObject;
+    
+    /**
+    * The create method put a visual of the animal on the screen
+    */
+    this.create = function(){
+        this.animal_on_page = document.createElement("div");
+        this.animal_on_page.className = "squirrel";
+        this.animal_on_page.id = this.id;
+        this.animal_on_page.style.left = this.x + "px";
+        this.animal_on_page.style.top = this.y + "px";
+        //put the div on the page as a child of the body
+        document.body.appendChild(this.animal_on_page);
+        thisObject = this;
+        thisObject.moveInt =setInterval(function(){
+            thisObject.squirrelBehavior(); 
+        },1000);
+    }//end function create();
+	
+    /**
+    *   The moveUpRight method makes the object move up and to the right on the movement grid
+    */
+    this.moveUpRight = function(){
+        for(i=0;i<50;i++){
+            $("#"+this.id).animate({left:'+=2px'},1); //squirrel object moves to the right 2px
+            $("#"+this.id).animate({top:'-=2px'},1); //squirrel object moves up 2px
+        }
+        this.rightCounter++;
+        this.leftCounter--;
+        this.upCounter++;
+        this.downCounter--;
+        console.log("I moved up and to the right");
+    }
+    
+    /**
+    *   The moveUpLeft method makes the object move up and to the left on the movement grid
+    */
+    this.moveUpLeft = function(){
+        for(i=0;i<50;i++){
+            $("#"+this.id).animate({left:'-=2px'},1); //squirrel object moves to the left 2px
+            $("#"+this.id).animate({top:'-=2px'},1); //squirrel object moves up 2px
+        }
+        this.rightCounter--;
+        this.leftCounter++;
+        this.upCounter++;
+        this.downCounter--;
+        console.log("I moved up and to the left");
+    }
+    
+    /**
+    *   The moveDownRight method makes the object move down and to the right on the movement grid
+    */
+    this.moveDownRight = function(){
+        for(i=0;i<50;i++){
+            $("#"+this.id).animate({left:'+=2px'},1); //squirrel object moves to the right 2px
+            $("#"+this.id).animate({top:'+=2px'},1); //squirrel object moves down 2px
+        }
+        this.rightCounter++;
+        this.leftCounter--;
+        this.upCounter--;
+        this.downCounter++;
+        console.log("I moved down and to the right");
+    }
+    
+    /**
+    *   The moveDownLeft method makes the object move down and to the left on the movement grid
+    */
+    this.moveDownLeft = function(){
+        for(i=0;i<50;i++){
+            $("#"+this.id).animate({left:'-=2px'},1); //squirrel object moves to the left 2px
+            $("#"+this.id).animate({top:'+=2px'},1); //squirrel object moves down 2px
+        }
+        this.rightCounter--;
+        this.leftCounter++;
+        this.upCounter--;
+        this.downCounter++;
+        console.log("I moved down and to the left");
+    }
+    
+    /**
+    * The moveRight method makes the object move to the right on the movement grid
+    */
+	this.moveRight = function(){
+        $("#"+this.id).animate({left:'+=100px'},1250); //squirrel object moves to the right 100px
+        this.rightCounter++;
+        this.leftCounter--;
+        console.log("I moved right");
+    }
+    
+    /**
+    * The moveLeft method makes the object move to the left on the movement grid
+    */
+    this.moveLeft = function(){
+        $("#"+this.id).animate({left:'-=100px'},1250); //squirrel object moves to the left 100px
+        this.leftCounter++;
+        this.rightCounter--;
+        console.log("I moved left");
+    }
+	
+    /**
+    * The moveUp method makes the object move up on the movement grid
+    */
+    this.moveUp = function(){
+        $("#"+this.id).animate({top:'-=100px'},1250); //squirrel object moves up 100px
+        this.upCounter++;
+        this.downCounter--;
+        console.log("I moved up");
+    }
+    
+    /**
+    * The moveDown method makes the object move down on the movement grid
+    */
+    this.moveDown = function(){
+        $("#"+this.id).animate({top:'+=100px'},1250); //squirrel object moves down 100px
+        this.downCounter++;
+        this.upCounter--;
+        console.log("I moved down"); 
+    }
+    
+    /**
+    * The squirrelBehavior method checks the values of the movement variables and builds the behaveArray array based upon their values. 
+    * If the conditions are met for the if statements, that particular movement function is made available to the object.
+    * A random number is then generated based upon the length of the of the behaveArray array.
+    * ??
+    */
+    this.squirrelBehavior = function(){
+         if (this.leftCounter < 3) {
+            this.behaveArray.push(1);
+         }
+        if (this.rightCounter < 3) {
+            this.behaveArray.push(2);
+        }
+        if (this.upCounter < 3) {
+            this.behaveArray.push(3);
+        }
+        if (this.downCounter < 3) {
+            this.behaveArray.push(4);
+        }
+        if (this.upCounter < 3 && this.rightCounter < 3) {
+            this.behaveArray.push(5);   
+        }
+        if (this.upCounter < 3 && this.leftCounter < 3) {
+            this.behaveArray.push(6);   
+        }
+        if (this.downCounter < 3 && this.rightCounter < 3) {
+            this.behaveArray.push(7);   
+        }
+        if (this.downCounter < 3 && this.leftCounter < 3) {
+            this.behaveArray.push(8);   
+        }
+    
+        this.behaveIndex = (Math.floor(Math.random()*this.behaveArray.length));
 
-function moveRight(){
-    $("#ghost").animate({right:'-=100px'},500); //ghost object moves to the right 100px
-    rightCounter++;
-    leftCounter--;
+        console.log('ba2:'+this.behaveIndex);
+        switch(this.behaveArray[this.behaveIndex]) {
+            case 1:
+                this.moveLeft();
+            break;
+            case 2:
+                this.moveRight();
+            break;
+            case 3:
+                this.moveUp();
+            break;
+            case 4:
+                this.moveDown();
+            break;
+            case 5:
+                this.moveUpRight();
+            break;
+            case 6:
+                this.moveUpLeft();
+            break;
+            case 7:
+                this.moveDownRight();
+            break;
+            case 8:
+                this.moveDownLeft();
+            break;
+            default:
+            break;
+        }
+    
+    console.log("Behave Array #: "+this.behaveArray, "Behave Index #: "+this.behaveIndex);
+    console.log("Up Counter: "+this.upCounter);
+    console.log("Down Counter: "+this.downCounter);
+    console.log("Left Counter: "+this.leftCounter);
+    console.log("Right Counter: "+this.rightCounter);
+    
+    this.behaveArray = [];
+    this.behaveIndex = 0;
+    }
 }
-
-function moveLeft(){
-    $("#ghost").animate({right:'+=100px'},500); //ghost object moves to the left 100px
-    leftCounter++;
-    rightCounter--;
-}
-
-function moveUp() {
-    $("#ghost").animate({top:'-=100px'},500); //ghost object moves up 100px
-    upCounter++;
-    downCounter--;
-}
-
-function moveDown() {
-    $("#ghost").animate({top:'+=100px'},500); //ghost object moves down 100px
-    downCounter++;
-    upCounter--;
-}
-
-function ghostBehavior() {
-    if (leftCounter < 3) {
-        behaveArray.push(1);
-    }
-    
-    if (rightCounter < 3) {
-        behaveArray.push(2);
-    }
-    
-    if (upCounter < 3) {
-        behaveArray.push(3);
-    }
-    
-    if (downCounter < 3) {
-        behaveArray.push(4);
-    }
-    
-    behaveIndex = (Math.floor(Math.random()*behaveArray.length + 1));
-    
-    console.log("counter is: "+counter);
-    console.log("leftCounter is: "+leftCounter);
-    console.log("rightCounter is: "+rightCounter);
-    console.log("upCounter is: "+upCounter);
-    console.log("downCounter is: "+downCounter);
-    console.log("behaveIndex is: "+behaveIndex);
-    
-    switch(behaveIndex) {
-        case 1:
-            moveRight();
-        break;
-        case 2:
-            moveLeft();
-        break;
-        case 3:
-            moveUp();
-        break;
-        case 4:
-            moveDown();
-        break;
-        default:
-        break;
-    }
-    
-    behaveIndex = 0;
-    behaveArray = [];
-}
-
-onload=init;
-
-function init(){
-    
-    while(counter<10){
-        ghostBehavior();
-        counter++;
-    }
-    
-//    console.log(upCounter);
-//    console.log(downCounter);
-//    console.log(leftCounter);
-//    console.log(rightCounter);
-    
-};
